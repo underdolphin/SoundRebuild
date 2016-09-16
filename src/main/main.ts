e7621f525dea2102217458c152f3f2df4a733abb
underdolphin <underdolphin@users.noreply.github.com>
2016-09-17 01:10:10 +0900

    Add cooperation with express.
    Added cooperation with express in main.js.

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,14 +1,27 @@
 /// <reference path="../../typings/index.d.ts" />
 
-import {app,BrowserWindow} from 'electron';
+import {app, BrowserWindow} from 'electron';
 const client = require('electron-connect').client;
+
+import * as express from 'express';
+import * as path from 'path';
+const exapp = express();
+const port = 3000;
+
+exapp.use(express.static(path.join(__dirname,'../renderer')));
+let server = exapp.listen(port,() => {
+    console.log(`app listening`);
+});
 
 let win: Electron.BrowserWindow = null;
 
 function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-    win.loadURL(`file://${__dirname}/../renderer/index.html`);
+    win.loadURL(`http://localhost:${port}`);
 
     win.on('closed', () => {
         win = null;
@@ -20,5 +33,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    app.quit();
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
