e9003e10b0cbc145486537647986183fc4bda434
underdolphin <underdolphin@users.noreply.github.com>
2016-09-18 16:35:43 +0900

    Change systemconfig.ts

--- src/renderer/script/systemconfig.ts
+++ src/renderer/script/systemconfig.ts
@@ -1,7 +1,12 @@
 /// <reference path="../../../typings/index.d.ts" />
 
 System.config({
-    defaultJSExtensions:true
+    baseURL: '.',
+    defaultJSExtensions: true,
+    map: {
+        settings: 'script/settings'
+    },
 });
 
 System.import('vue');
+System.import('settings/main');
