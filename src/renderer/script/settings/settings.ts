e33a1cd26c593b58046b73d61bb8abcd9c299289
underdolphin <underdolphin@users.noreply.github.com>
2016-09-29 23:11:42 +0900

    Fix reference path of typings

--- src/renderer/script/settings/settings.ts
+++ src/renderer/script/settings/settings.ts
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-/// <reference path="../../../typings/index.d.ts" />
+/// <reference path="../../../../typings/index.d.ts" />
 
 Polymer({
     is: "settings-element"
