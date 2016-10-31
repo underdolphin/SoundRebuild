ac91b4303178310ab9686245b9391c619f6de5e8
underdolphin <underdolphin@users.noreply.github.com>
2016-10-31 23:34:09 +0900

    Revert "Fix typo and indent"
    This reverts commit a300e7879014a18b19fa07b68672004b89002722.

--- src/renderer/script/settings/settings.ts
+++ src/renderer/script/settings/settings.ts
@@ -13,30 +13,5 @@
 //    limitations under the License.
 
 Polymer({
-    is: "settings-element",
-    play: () => {
-        // get theme elements
-        const lr2StyleDirectory =
-            document.getElementById('lr2_style_dir_label') as HTMLInputElement;
-        const lr2StyleEncodings =
-            document.getElementsByName('lr2_style_encoding') as NodeListOf<HTMLInputElement>;
-
-        // get songs elements
-        const bmsStyleDirectories =
-            document.getElementById('bms_style_dirs_label') as HTMLInputElement;
-        const bmsStyleSongs =
-            document.getElementById('bms_style_songs_label') as HTMLInputElement;
-        const bmsStyleEncodings =
-            document.getElementsByName('bms_style_encoding') as NodeListOf<HTMLInputElement> ;
-
-        // get options elements
-        const selectWindowModeButton = document.getElementById('option_window_mode_window') as HTMLInputElement;
-        const selectFullscreenModeButton = document.getElementById('option_window_mode_fullscreen') as HTMLInputElement;
-        const soundMasterVolume = document.getElementById('option_sound_master_volume') as HTMLInputElement;
-        const selectWebAudioOutputButton = document.getElementById('option_sound_output_webaudio') as HTMLInputElement;
-        const selectKeyboardInputButton = document.getElementById('option_others_input_keyboard') as HTMLInputElement;
-        const selectWebMidiInputButton = document.getElementById('option_others_input_webmidi') as HTMLInputElement;
-
-        console.log(lr2StyleDirectory.value);
-    }
+    is: "settings-element"
 });
