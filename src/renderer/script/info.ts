1808fcd8f390edc4c4f0832195f826dee052b22e
underdolphin <underdolphin@users.noreply.github.com>
2016-11-14 17:51:16 +0900

    Add software info

---
+++ src/renderer/script/info.ts
@@ -0,0 +1,6 @@
+export class Info {
+    public Organization = '';
+    public Title = 'Re:sound';
+    public Copyright = '© 2016 underdolphin(masato sueda)';
+    public Version = '2016.9.12-wip';
+}
